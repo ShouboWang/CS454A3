293c8238425b6830e5a965d89521d66b224eed12
Shoubo Wang <sbwang@uwaterloo.ca>
2016-07-06 22:08:54 -0400

    fixed indentation

--- rpc.cc
+++ rpc.cc
@@ -28,15 +28,15 @@ int rpc_sock_port;
 int rpcrpcInit()
 {
 	// Create socket for client
-  struct addrinfo rpc_hints, *rpc_ai;	// Address info
+	struct addrinfo rpc_hints, *rpc_ai;	// Address info
 
-  // Set the address info
-  memset(&rpc_hints, 0, sizeof rpc_hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+	// Set the address info
+	memset(&rpc_hints, 0, sizeof rpc_hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
-  // Get address info
+	// Get address info
 	getaddrinfo(NULL, "0", &rpc_hints, &rpc_ai);
 	rpc_sock = socket(rpc_ai->ai_family, rpc_ai->ai_socktype, rpc_ai->ai_protocol);
 	bind(rpc_sock, rpc_ai->ai_addr, rpc_ai->ai_addrlen);
@@ -53,11 +53,11 @@ int rpcrpcInit()
 	getnameinfo((struct sockaddr*)&sock_addr, &lengths, rpc_socket_id, 128, NULL, 0, 0);
 
 	// Get the rpc dock port
-  getsockname(rpc_sock, (struct sockaddr* ) &sock_addr, &sock_addr_len);
+	getsockname(rpc_sock, (struct sockaddr* ) &sock_addr, &sock_addr_len);
 	rpc_sock_port = ntohs(sock_addr.sin_port);
 
-  // Address info is no longer needed
-  freeaddrinfo(rpc_ai);
+	// Address info is no longer needed
+	freeaddrinfo(rpc_ai);
 
 	// Connect to binder
 	// Get Binder's address & port
@@ -66,20 +66,20 @@ int rpcrpcInit()
 
 	// Validates that the address and port is set
 	if(binder_address == NULL) {
-		return ERROR_BINDER_ADDRESS_NOT_FOUND;
+	return ERROR_BINDER_ADDRESS_NOT_FOUND;
 	}
 	if(binder_port == NULL) {
-		return ERROR_BINDER_PORT_NOT_FOUND;
+	return ERROR_BINDER_PORT_NOT_FOUND;
 	}
 
 	// Make connection to binder
 	int sockfd;
-  struct addrinfo binder_hints, *binder_ai;
+	struct addrinfo binder_hints, *binder_ai;
 
 	// Get the address info of binder
-  memset(&binder_hints, 0, sizeof binder_hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+	memset(&binder_hints, 0, sizeof binder_hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 	getaddrinfo(binder_address, binder_port, &binder_hints, &binder_ai);
 
 	// make connection to binder
